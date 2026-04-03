c51f95f68f128fc5e11a71a70d0b857c85de87df
Favour M <favourmadubuko1@gmail.com>
2026-04-03 20:11:25 +0000

    feat(facility-management): add performance page

---
+++ src/app/state-dashboard/facility-management/performance/page.tsx
@@ -0,0 +1,5 @@
+import FacilityPerformance from "@/src/components/stateDashboard/facility-management/FacilityPerformance";
+
+export default function PerformancePage() {
+  return <FacilityPerformance />;
+}
