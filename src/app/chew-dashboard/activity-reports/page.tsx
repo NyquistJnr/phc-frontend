fd85e296769b259aca55a7583e1b0036a8e8b6b4
Favour M <favourmadubuko1@gmail.com>
2026-05-06 19:18:37 +0100

    feat(ChewActivityReportsPage): create activity reports page

---
+++ src/app/chew-dashboard/activity-reports/page.tsx
@@ -0,0 +1,5 @@
+import ChewActivityReports from "@/src/components/chewDashboard/activityReports/ChewActivityReports";
+
+export default function ChewActivityReportsPage() {
+  return <ChewActivityReports />;
+}
