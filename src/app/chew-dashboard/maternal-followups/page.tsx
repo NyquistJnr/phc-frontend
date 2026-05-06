f126c40c8267b0910e04ac5cbef67a07adc3c1dd
Favour M <favourmadubuko1@gmail.com>
2026-05-06 19:15:43 +0100

    feat(ChewMaternalFollowUpsPage): add maternal follow-ups page component

---
+++ src/app/chew-dashboard/maternal-followups/page.tsx
@@ -0,0 +1,5 @@
+import ChewMaternalFollowUps from "@/src/components/chewDashboard/maternalFollowUps/ChewMaternalFollowUps";
+
+export default function ChewMaternalFollowUpsPage() {
+  return <ChewMaternalFollowUps />;
+}
