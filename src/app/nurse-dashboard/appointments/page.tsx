44af48c445acf99c7b49dece6453a7717486bd6c
Nyquist Nwaukwa <nyquist.nwaukwa@gmail.com>
2026-05-07 23:14:20 +0100

    Fixed the build issues.

--- src/app/nurse-dashboard/appointments/page.tsx
+++ src/app/nurse-dashboard/appointments/page.tsx
@@ -1,5 +1,10 @@
 import Appointments from "@/src/components/nurse-dashboard/appointments/Appointments";
+import { Suspense } from "react";
 
 export default function AppointmentsPage() {
-  return <Appointments />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Appointments />
+    </Suspense>
+  );
 }
