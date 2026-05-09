caf2b423086a9717c1c04b9a7c59b64395dcfaff
Nyquist Nwaukwa <nyquist.nwaukwa@gmail.com>
2026-05-09 18:04:38 +0100

    Fixed the build errors.

---
+++ src/app/nurse-dashboard/patients/new/page.tsx
@@ -0,0 +1,5 @@
+import CreateNewPatient from "@/src/components/nurse-dashboard/patients/CreatePatient";
+
+export default function CreateNewPatientPage() {
+  return <CreateNewPatient />;
+}
---
+++ src/components/nurse-dashboard/patients/CreatePatient.tsx
@@ -0,0 +1,3 @@
+export default function CreateNewPatient() {
+  return <div>Hello</div>;
+}
