2c33eca81776d2e200469f2d29e2eeb81606f200
Nyquist Nwaukwa <nyquist.nwaukwa@gmail.com>
2026-05-10 16:06:51 +0100

    Fixed build errors.

--- src/app/dashboard/user-management/page.tsx
+++ src/app/dashboard/user-management/page.tsx
@@ -1,5 +1,12 @@
+"use client";
+
+import { Suspense } from "react";
 import UserManagement from "@/src/components/adminDashboard/user-management/Home";
 
 export default function UserManagementPage() {
-  return <UserManagement />;
+  return (
+    <Suspense fallback={<div>Loading</div>}>
+      <UserManagement />
+    </Suspense>
+  );
 }
--- src/app/lab-dashboard/page.tsx
+++ src/app/lab-dashboard/page.tsx
@@ -1,5 +1,12 @@
+"use client";
+
+import { Suspense } from "react";
 import LabHome from "@/src/components/lab-dashboard/home/LabHome";
 
 export default function LabDashboardPage() {
-  return <LabHome />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <LabHome />
+    </Suspense>
+  );
 }
